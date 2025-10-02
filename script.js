188f1be2bc3b8a4ba18015a862d31d7f0a9a7c26
alygatougozaimasuu <aditamaarsyi@gmail.com>
2025-10-02 19:40:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ const sounds = {
     click: new Audio('https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3'),
     skill: new Audio('https://assets.mixkit.co/active_storage/sfx/2019/2019-preview.mp3'),
     zonk: new Audio('https://assets.mixkit.co/active_storage/sfx/2573/2573-preview.mp3'),
-    win: new Audio('https://raw.githubusercontent.com/altschmerzsiuu/tic-tac-toe/main/tenxi.mp3'),
+    win: new Audio('https://raw.githubusercontent.com/altschmerzsiuu/tic-tac-toe/main/mejikuhibiniu.mp3'),
     timer: new Audio('https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3')
 };
 // placeholder for countdown (you'll replace with your github raw url)
-const countdownSound = new Audio('https://raw.githubusercontent.com/altschmerzsiuu/tic-tac-toe/main/new.mp3'); // <-- replace this path with your GitHub URL
+const countdownSound = new Audio('https://raw.githubusercontent.com/altschmerzsiuu/tic-tac-toe/main/cd.mp3');
 countdownSound.volume = 1.0;
 
 document.addEventListener('click', () => {
